d5fa4e83fdd0902cdad24b33f74a520789fd9749
Huzaim Malik <huzaimm01@gmail.com>
2024-11-05 10:39:31 -0500

    Create hamburger.js

---
+++ hamburger.js
@@ -0,0 +1,13 @@
+<!-- Hamburger icon -->
+<div class="hamburger" id="hamburgerIcon">☰</div>
+
+<!-- Fullscreen menu for small screens -->
+<div class="fullscreen-menu" id="fullscreenMenu">
+  <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="aboutme.html">About Me</a></li>
+    <li><a href="resume.html">Résumé</a></li>
+    <li><a href="projectpage.html">Projects</a></li>
+    <li><a href="contact.html">Contact</a></li>
+  </ul>
+</div>
